ecd5958c77eff4dd837bfe775e7fe6d75310042d
sandeepSpark <srijanup@outlook.com>
2021-04-26 16:24:38 +0545

    workshow case section done

--- wp-content/themes/crupee/front-page.php
+++ wp-content/themes/crupee/front-page.php
@@ -10,4 +10,32 @@ $context = Timber::context();
 
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
+
+$workshowcase_category = get_terms('workshowcase_cat');
+$context['workshowcase_category'] = $workshowcase_category;
+
+$works = array();
+foreach ($workshowcase_category as $key => $value) {
+	$args = array(
+		'post_type' => 'works',
+		'post_status' => 'publish',
+		'posts_per_page' => -1,
+		// 'orderby' => 'meta_value_num',
+		// 'meta_key' => 'works_priority',
+		'order' => 'DESC',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'workshowcase_cat',
+				'field' => ' term_id',
+				'terms' => $value->term_id,
+			),
+		),
+	);
+	$works[$value->slug] = Timber::get_posts($args);
+}
+$context['workshowcase'] = $works;
+// echo "<pre>";
+// print_r($workshowcase_category);
+// print_r($works);
+// die;
 Timber::render(array('home.twig', 'page.twig'), $context);
--- wp-content/themes/crupee/functions.php
+++ wp-content/themes/crupee/functions.php
@@ -14,7 +14,7 @@
  * plug-in, you can safely delete this block.
  */
 $composer_autoload = __DIR__ . '/vendor/autoload.php';
-if ( file_exists( $composer_autoload ) ) {
+if (file_exists($composer_autoload)) {
 	require_once $composer_autoload;
 	$timber = new Timber\Timber();
 }
@@ -23,18 +23,18 @@ if ( file_exists( $composer_autoload ) ) {
  * This ensures that Timber is loaded and available as a PHP class.
  * If not, it gives an error message to help direct developers on where to activate
  */
-if ( ! class_exists( 'Timber' ) ) {
+if (!class_exists('Timber')) {
 
 	add_action(
 		'admin_notices',
-		function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		function () {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
 		}
 	);
 
 	add_filter(
 		'template_include',
-		function( $template ) {
+		function ($template) {
 			return get_stylesheet_directory() . '/static/no-timber.html';
 		}
 	);
@@ -44,14 +44,19 @@ if ( ! class_exists( 'Timber' ) ) {
 /**
  * Sets the directories (inside your theme) to find .twig files
  */
-Timber::$dirname = array( 'templates', 'views' );
+Timber::$dirname = array('templates', 'views');
+date_default_timezone_set('Asia/Kathmandu');
+define('CRUPEESOFT_THEME_DIR', dirname(__FILE__));
+define('CRUPEESOFT_THEME_URL', get_template_directory_uri());
+
+require_once CRUPEESOFT_THEME_DIR . '/inc/post_types.php'; /*including registered custom post types*/
+require_once CRUPEESOFT_THEME_DIR . '/inc/taxonomy_register.php'; /*including registered custom taxonomy types*/
 
 /**
  * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
  * No prob! Just set this value to true
  */
 Timber::$autoescape = false;
-
 
 /**
  * We're going to configure our theme inside of a subclass of Timber\Site
@@ -60,11 +65,11 @@ Timber::$autoescape = false;
 class StarterSite extends Timber\Site {
 	/** Add timber support. */
 	public function __construct() {
-		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
-		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
-		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		add_action('after_setup_theme', array($this, 'theme_supports'));
+		add_filter('timber/context', array($this, 'add_to_context'));
+		add_filter('timber/twig', array($this, 'add_to_twig'));
+		add_action('init', array($this, 'register_post_types'));
+		add_action('init', array($this, 'register_taxonomies'));
 		parent::__construct();
 	}
 	/** This is where you can register custom post types. */
@@ -80,38 +85,38 @@ class StarterSite extends Timber\Site {
 	 *
 	 * @param string $context context['this'] Being the Twig's {{ this }}.
 	 */
-	public function add_to_context( $context ) {
-		$context['foo']   = 'bar';
+	public function add_to_context($context) {
+		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
-		$context['site']  = $this;
+		$context['menu'] = new Timber\Menu();
+		$context['site'] = $this;
 		return $context;
 	}
 
 	public function theme_supports() {
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+			 * Let WordPress manage the document title.
+			 * By adding theme support, we declare that this theme does not use a
+			 * hard-coded <title> tag in the document head, and expect WordPress to
+			 * provide it for us.
+		*/
+		add_theme_support('title-tag');
 
 		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+			 * Enable support for Post Thumbnails on posts and pages.
+			 *
+			 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		*/
+		add_theme_support('post-thumbnails');
 
 		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
+			 * Switch default core markup for search form, comment form, and comments
+			 * to output valid HTML5.
+		*/
 		add_theme_support(
 			'html5',
 			array(
@@ -123,10 +128,10 @@ class StarterSite extends Timber\Site {
 		);
 
 		/*
-		 * Enable support for Post Formats.
-		 *
-		 * See: https://codex.wordpress.org/Post_Formats
-		 */
+			 * Enable support for Post Formats.
+			 *
+			 * See: https://codex.wordpress.org/Post_Formats
+		*/
 		add_theme_support(
 			'post-formats',
 			array(
@@ -140,14 +145,14 @@ class StarterSite extends Timber\Site {
 			)
 		);
 
-		add_theme_support( 'menus' );
+		add_theme_support('menus');
 	}
 
 	/** This Would return 'foo bar!'.
 	 *
 	 * @param string $text being 'foo', then returned 'foo bar!'.
 	 */
-	public function myfoo( $text ) {
+	public function myfoo($text) {
 		$text .= ' bar!';
 		return $text;
 	}
@@ -156,9 +161,9 @@ class StarterSite extends Timber\Site {
 	 *
 	 * @param string $twig get extension.
 	 */
-	public function add_to_twig( $twig ) {
-		$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
-		$twig->addFilter( new Twig\TwigFilter( 'myfoo', array( $this, 'myfoo' ) ) );
+	public function add_to_twig($twig) {
+		$twig->addExtension(new Twig\Extension\StringLoaderExtension());
+		$twig->addFilter(new Twig\TwigFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
 	}
 
