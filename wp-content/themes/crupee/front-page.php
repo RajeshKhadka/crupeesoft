21807458dd0624d8137a41bf8e6744f7ebdac41c
sandeepSpark <srijanup@outlook.com>
2021-04-26 17:12:14 +0545

    showcase section done and slider system partially done

--- wp-content/themes/crupee/front-page.php
+++ wp-content/themes/crupee/front-page.php
@@ -34,6 +34,17 @@ foreach ($workshowcase_category as $key => $value) {
 	$works[$value->slug] = Timber::get_posts($args);
 }
 $context['workshowcase'] = $works;
+
+$args = array(
+	'post_type' => 'sliders',
+	'post_status' => 'publish',
+	'posts_per_page' => -1,
+	// 'orderby' => 'meta_value_num',
+	// 'meta_key' => 'works_priority',
+	'order' => 'DESC',
+
+);
+$context['sliders'] = Timber::get_posts($args);
 // echo "<pre>";
 // print_r($workshowcase_category);
 // print_r($works);
