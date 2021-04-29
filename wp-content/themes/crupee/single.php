56b12602704314d55ba17a60cebb6ba6e49727c4
sandeepSpark <srijanup@outlook.com>
2021-04-29 12:19:53 +0545

    home page section

--- wp-content/themes/crupee/single.php
+++ wp-content/themes/crupee/single.php
@@ -9,12 +9,23 @@
  * @since    Timber 0.1
  */
 
-$context         = Timber::context();
-$timber_post     = Timber::get_post();
+$context = Timber::context();
+$timber_post = Timber::get_post();
 $context['post'] = $timber_post;
 
-if ( post_password_required( $timber_post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
+$args = array(
+	'post_type' => 'post',
+	'post_status' => 'publish',
+	'posts_per_page' => 5,
+	// 'orderby' => 'meta_value_num',
+	// 'meta_key' => 'works_priority',
+	'order' => 'DESC',
+
+);
+$context['latest_news'] = Timber::get_posts($args);
+
+if (post_password_required($timber_post->ID)) {
+	Timber::render('single-password.twig', $context);
 } else {
-	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig' ), $context );
+	Timber::render(array('single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig'), $context);
 }
